496edbace352903277e28d4aab2515167af338d1
whisper612 <mr.way;ander@bk.ru>
2022-10-24 21:58:14 +0700

    Revert "Ignore fix"
    This reverts commit 49f7f6b458ba03663c67924ffdd8f8e3d1153cce.

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,26 @@ import (
 	"github.com/whisper612/logger/internal/someservice"
 	cl "github.com/whisper612/logger/pkg/logger/consolelogger"
 	fl "github.com/whisper612/logger/pkg/logger/filelogger"
-	tg "github.com/whisper612/logger/pkg/logger/telegramlogger"
 )
 
 func main() {
 	consoleLogger := &cl.ConsoleLogger{}
 	fileLogger := &fl.FileLogger{}
-	telegramLogger := &tg.TelegramLogger{}
+	// telegramLogger := &cl.TelegramLogger{}
 
 	service0 := someservice.NewService(consoleLogger)
 	service1 := someservice.NewService(fileLogger)
-	service2 := someservice.NewService(telegramLogger)
+	// service2 := someservice.NewService(telegramLogger)
 
 	service0.Serve()
 	service1.Serve()
-	service2.Serve()
+	// service2.Serve()
 }
+
+/*
+	@TODO
+	----------    ДЗ    ----------
+	1*) стектрейс в логе
+	2) логгер в файл
+	3) логгер куда-нибудь на сервер
+*/
